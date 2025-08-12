7a243908ef321181c4c46de770ad680a74d7a130
blinkinfotech007 <blinkinfotech007@gmail.com>
2025-08-12 19:39:42 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -13,20 +13,11 @@ const companyInfo = {
     facebook: "https://www.facebook.com/profile.php?id=61578623747029",
     twitter: "https://x.com/blinkinfo007"
   },
-  teamMembers: [
-    {
-      name: "Raj Bhanderi",
-      role: "Android Developer (CEO)",
-      image: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-      link: "https://example.com/raj"
-    },
-    {
-      name: "Dhruv Bhanderi",
-      role: "Android/Flutter Developer (CEO)",
-      image: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-      link: "https://dhruvbhanderi4868.github.io/"
-    }
-  ]
+  apis: {
+    blog: "https://api.npoint.io/5fea83f23a9e73f5863b",
+    reviews: "https://api.npoint.io/999920066ff2d4219a68",
+    team: "https://api.npoint.io/147a9f707c6307fad1a1"
+  },
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -39,18 +30,16 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("a.instagram").forEach(el => el.href = companyInfo.socials.instagram);
   document.querySelectorAll("a.linkedin").forEach(el => el.href = companyInfo.socials.linkedin);
   document.querySelectorAll("a.facebook").forEach(el => el.href = companyInfo.socials.facebook);
-   document.querySelectorAll("a.twitter").forEach(el => el.href = companyInfo.socials.twitter);
+  document.querySelectorAll("a.twitter").forEach(el => el.href = companyInfo.socials.twitter);
 
-  document.querySelectorAll(".contact-form").forEach(form => {
-    form.action = companyInfo.contactFormAction;
-  });
+  document.querySelectorAll(".contact-form").forEach(form => form.action = companyInfo.contactFormAction);
 
   const logoContainers = document.querySelectorAll(".company-logo");
   logoContainers.forEach(container => {
     const img = document.createElement("img");
     img.src = companyInfo.logo;
     img.alt = `${companyInfo.name} Logo`;
-    img.className = "logo-img"; // Add your CSS class
+    img.className = "logo-img";
     container.prepend(img);
   });
 
@@ -59,23 +48,4 @@ document.addEventListener("DOMContentLoaded", () => {
   favicon.href = companyInfo.favicon;
   favicon.type = "image/png";
   document.head.appendChild(favicon);
-
-  // Generate team section
-  const teamContainer = document.querySelector("#team .grid");
-  if (teamContainer && companyInfo.teamMembers) {
-    companyInfo.teamMembers.forEach(member => {
-      const card = document.createElement("div");
-      card.className = "card team-card";
-      card.onclick = () => window.open(member.link, "_blank");
-
-      card.innerHTML = `
-        <img src="${member.image}" alt="${member.name}" class="team-avatar">
-        <h4>${member.name}</h4>
-        <p>${member.role}</p>
-      `;
-
-      teamContainer.appendChild(card);
-    });
-  }
 });
-
